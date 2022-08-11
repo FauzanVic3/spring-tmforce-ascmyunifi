e9989cd5c84d103d208151fed2b142aea16a6eeb
MUHAMMAD FAUZAN BIN MUHAMMAD FAUZI <36467163+FauzanVic3@users.noreply.github.com>
2022-08-12 02:07:12 +0800

    Added Response and Test

--- src/main/java/my/com/fauzanvic3/spring/tmforce/ascmyunifi/resource/ProgressUpdateResource.java
+++ src/main/java/my/com/fauzanvic3/spring/tmforce/ascmyunifi/resource/ProgressUpdateResource.java
@@ -9,9 +9,12 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.client.Client;
+import javax.ws.rs.core.MediaType;
 import my.com.fauzanvic3.spring.tmforce.ascmyunifi.AscmyunifiResource;
 import my.com.fauzanvic3.spring.tmforce.ascmyunifi.service.ProgressUpdateService;
 
@@ -22,6 +25,7 @@ import my.com.tmrnd.tmforce.common.api.json.model.dac.MyUnifiProgressUpdateMainR
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -46,15 +50,15 @@ public class ProgressUpdateResource extends AscmyunifiResource{
     
     @PostMapping("/progressupdate")
 //    @Path("/progressupdate")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
 //    @UnitOfWork
     @ApiOperation(value = "Get Progress Update")
     @ApiResponses(value = {
-        @ApiResponse(code = 200, message = "Success", response = MyUnifiProgressUpdateMainRequest.class),
+        @ApiResponse(code = 200, message = "Success", response = MyUnifiProgressUpdateMainResponse.class),
         @ApiResponse(code = 400, message = "Error 400, 404, 500", response = ExceptionResponse.class)
     })
-    public MyUnifiProgressUpdateMainResponse progressUpdate(@HeaderParam(TMF_REQUEST) String requestId, MyUnifiProgressUpdateMainRequest request) throws WebApplicationException{
+    public MyUnifiProgressUpdateMainResponse progressUpdate(@HeaderParam(TMF_REQUEST) String requestId, @RequestBody MyUnifiProgressUpdateMainRequest request) throws WebApplicationException{
 
         MyUnifiProgressUpdateMainResponse response = service.progressUpdate(requestId, request);
            
